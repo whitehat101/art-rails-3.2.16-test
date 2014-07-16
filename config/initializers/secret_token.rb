2c165283d65bc50853ecd86aa18986b1f8bba144
Jeremy Ebler <jebler@gmail.com>
2014-07-15 19:27:19 -0700

    Rails new 3.2.16

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ARTTest::Application.config.secret_token = 'cb4c0755a1e5c9555ae863ab6169e2ab5ff89e3e660d7602ab88762a08173bf162d192225c72b53809d2830805d9f66e4a7c67efae658b24804d28edfc5ef989'
